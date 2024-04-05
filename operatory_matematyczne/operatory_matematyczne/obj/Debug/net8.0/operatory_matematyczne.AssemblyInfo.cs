30163420686504b5f90cf3e2012d7e6d37e19f05
Rafał Pociecha <rafal.pociecha.rp@gmail.com>
2024-04-05 10:38:17 +0200

    l

--- operatory_matematyczne/operatory_matematyczne/obj/Debug/net8.0/operatory_matematyczne.AssemblyInfo.cs
+++ operatory_matematyczne/operatory_matematyczne/obj/Debug/net8.0/operatory_matematyczne.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("operatory_matematyczne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef407d4318e8349125df241f5285d8738a33ad2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fe2a2a7f51c2a7e6c40c2c5c72684ad89ff1122")]
 [assembly: System.Reflection.AssemblyProductAttribute("operatory_matematyczne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("operatory_matematyczne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
